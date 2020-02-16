f74bb01716458083c0a744b5200b8414c8d36f8e
alex <barmaleyalex@gmail.com>
2020-02-16 12:38:57 -0800

    added header and its styles,todo:add responsive styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
@@ -10,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route  path='/shop' component={ShopPage}/>
