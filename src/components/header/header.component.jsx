924699be1e4e11e6f2284209a765314140e1b3bb
alex <barmaleyalex@gmail.com>
2020-02-19 21:59:25 -0800

    added cart redux,and added hide and show functionality to the clickable icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,10 @@ import {ReactComponent as Logo} from '../../assets/DeerLogo.svg';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header =({currentUser})=>(
+const Header =({currentUser,hidden})=>(
     <div className='header'>
          <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -22,13 +23,24 @@ const Header =({currentUser})=>(
             }
             <CartIcon/>
          </div>
+        { 
+      //   if tru render nothing if not render the cart
+         hidden ? null :
+        <CartDropdown/>
+        }
     </div>
 );
 
 // state is rootreducer top level
 // we want rootreducer -> goes to userReducer -> and gets currentUser
-const mapStateToProps = state =>({
-   currentUser: state.user.currentUser
-})
+
+// const mapStateToProps = state =>({
+//    currentUser: state.user.currentUser
+// })
+
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) =>({
+   currentUser,
+   hidden
+});
 
 export default connect(mapStateToProps)( Header);
