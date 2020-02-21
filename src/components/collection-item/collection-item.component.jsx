733a3e44092db008ba1271132de3034abf61b60e
alex <barmaleyalex@gmail.com>
2020-02-20 23:09:03 -0800

    added item counter in icon component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import CustomButton from '../custom-button/custom-button.component';
+
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
+
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item,addItem }) => {
+  const {name,price,imageUrl} = item;
+  return(
   <div className="collection-item">
     <div
       className="image"
@@ -11,6 +18,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton onClick={()=>addItem(item)} inverted>Add to Cart</CustomButton>
   </div>
-);
-export default CollectionItem;
+)};
+
+
+const mapDispatchToProps = dispatch =>({
+  // prop addItem that will take tiem as property and pass it onto our addItem action creator
+  addItem: item => dispatch(addItem(item))
+})
+
+
+export default connect(null,mapDispatchToProps) (CollectionItem);
