46fc603c0704bcfc25c4cf63d989aec05c3d7434
alex <barmaleyalex@gmail.com>
2020-02-25 19:03:18 -0800

    Added peristance library with local storage config

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import {BrowserRouter} from 'react-router-dom';
 
 import {Provider} from 'react-redux'
 
+// Persister
+import {PersistGate} from 'redux-persist/integration/react';
+import {store,persistor} from './redux/store';
+
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+// import store from './redux/store';
 
 
 ReactDOM.render(
 <Provider store={store}>
     <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
     </BrowserRouter>
 </Provider>
 , document.getElementById('root'));
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,31 @@
 // Overall reducer based on all of apps reducer
 import {combineReducers} from 'redux';
 
+import {persistReducer} from 'redux-persist';
+// Tell redux persist we want to use local storage as our default or u can use session storage
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+// Persist Config as JSON object
+const persistConfig = {
+    // from where you wanna start storing inside our reducer
+    key:'root',
+    // where key goes
+    storage,
+    // string names of any reducer we want to store///which reducer we want to persist
+    whitelist:['cart']
+
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+export default persistReducer(persistConfig,rootReducer);
+
+// export default combineReducers({
+//     user: userReducer,
+//     cart: cartReducer
+// });
